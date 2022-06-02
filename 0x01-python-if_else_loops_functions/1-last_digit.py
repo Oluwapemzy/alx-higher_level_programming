bcb71687e7c6d4fe3b81b29a25665983a940d19e
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-01 17:52:57 -0700

    last digit file sorted

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,5 +13,4 @@ elif lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0")
 
 elif lastDigit < 6 and lastDigit != 0:
-    print(f"Last digit of {number} is {lastDigit} \
-    \and is less than 6 and not 0")
+    print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
