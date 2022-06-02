839b8e44ef5f3e05bef6d0cf7c6c8df9e7e59a0d
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-02 07:50:09 -0700

    print arguments with python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    if len(argv) <= 1:
+        print("0 arguments")
+    else:
+        print("{:d} arguments.".format(len(argv) - 1))
+    for arg in range(1, len(argv)):
+        print("{:d}: {}".format(arg, sys.argv[arg]))
