5efddd6f142dd58058a9963ec2e4b59b2b07a8a3
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-02 17:52:44 -0700

    4-hidden dicovery refixed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     import hidden_4
-    for  name in dir(hidden_4):
+    for name in dir(hidden_4):
         if name[0] != "_" and name[1] != "_":
             print(name)
