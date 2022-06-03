e530467f7f08c7e27b945bfe5387d71f677bc8d1
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-02 17:37:39 -0700

    pycodestyle fixed for 100-calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if len_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     num1 = int(args[1])
     num2 = int(args[3])
     op_passed = args[2]
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     if op_passed not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    if op_passed =="+":
+    if op_passed == "+":
         print(f"{num1} + {num2} = {add(num1, num2)}")
     elif op_passed == '-':
         print(f"{num1} - {num2} = {sub(num1, num2)}")
