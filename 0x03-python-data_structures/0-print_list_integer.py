2bff9ecd668bd700fa8865484a2b4cb2d359c0e7
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-05 19:13:53 -0700

    added :d into curly braces

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for integer in range(len(my_list)):
-        print("{}".format(my_list[integer]), end="\n")
+        print("{:d}".format(my_list[integer]), end="\n")
