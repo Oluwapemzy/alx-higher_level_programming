995a5371587267361818198c1c4aea6497066b77
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-08 09:30:17 -0700

    search and replace done

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/python3
+def uniq_add(my_list=[]):
+    my_uniq = []
+    sum_uniq = 0
+    for item in my_list:
+        if item not in my_uniq:
+            my_uniq.append(item)
+    for uniq in my_uniq:
+        sum_uniq += uniq
+    return(sum_uniq)
