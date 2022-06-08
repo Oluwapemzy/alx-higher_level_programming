7eb971b2ee09cf1c55f78a56fb0f5020d6ba8d4a
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-08 09:24:10 -0700

    0-square list in list

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for sub_list in matrix:
+        squ_list = list(map((lambda x:x **2), sub_list))
+        new_matrix.append(squ_list)
+    return new_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,2 @@
+# This directory contains solutions to tasks on more Data Structires.
+#Manipulating the different data types structures such as set matrix(i.e list inside of a list)
