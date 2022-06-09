c6b563b2eb6adb83c793beaf25d945bcdf88c66b
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-09 04:27:23 -0700

    best score and multiply list by num using map

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    def times_num(the_list): return the_list * number
+    new_list = list(map(times_num, my_list))
+    return new_list
