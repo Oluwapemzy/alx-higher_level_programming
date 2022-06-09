7ec24430523791cb0ee49226e5de67d5fed8ffb3
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-09 04:48:55 -0700

    fixes on multiply without loops but map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    times_num = lambda the_list: the_list * number
-    new_list = list(map(times_num, my_list))
-    return new_list
+    return list(map(lambda lis: lis * number, my_list))
