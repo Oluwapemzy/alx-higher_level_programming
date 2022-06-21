b15285a91c69fec71dc0f00d202632253d633d38
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-21 02:03:45 -0700

    0-safe file refixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        for num in range(x):
+    for num in range(x):
+        try:
             print(my_list[num], end="")
             count += 1
     except:
