ec9a42dc049292e23e6ad75df87e99e4dad8f0a4
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-21 02:08:36 -0700

    fixed indentation error in o-safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[num]), end="")
             count += 1
-    except IndexError:
-        break
+        except IndexError:
+            break
 
     print("")
     return count
