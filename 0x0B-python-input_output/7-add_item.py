c5f935cb11b25c8f48036e5a085a26fc12131bc7
Oluwapemzy <ojeksman2000@gmail.com>
2022-07-05 09:49:45 -0700

    trying to fix task 7 file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,15 @@
 """Adds all arguments to a list and save to file"""
 import sys
 
-
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
 
 
     try:
-        add = load_from_json_file("add_item.json")
+        my_add = load_from_json_file("add_item.json")
     except FIleNotFoundError:
-        items = []
-    add.extend(sys.argv[1:])
-    save_to_json_file(add, "add_item.json")
+        my_add = []
+    my_add.extend(sys.argv[1:])
+    save_to_json_file(my_add, "add_item.json")
