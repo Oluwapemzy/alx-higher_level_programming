27ee9bdaef9cce7f36fb68218159068a17a01cfe
Oluwapemzy <ojeksman2000@gmail.com>
2022-08-09 20:06:50 -0700

    added bartick to table name and added space

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- script that creates a table second_table in the database and add multiple rows
 CREATE TABLE IF NOT EXISTs second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(`id`, `name`, score) VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, George, 8);
+INSERT INTO `second_table` (`id`, `name`, score) VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, George, 8);
