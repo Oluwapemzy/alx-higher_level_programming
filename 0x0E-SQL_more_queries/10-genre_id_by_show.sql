6b5bc9c46273c8c3d97a374317e999ac95ffcfe5
Oluwapemzy <ojeksman2000@gmail.com>
2022-08-10 21:43:11 -0700

    tasks 13 to 16 &10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- List all shows in 'hbtn_0d_tvshows' that have at least one genre linked
-SELECT c.`id`, c.`name`, s.`name`
-  FROM `cities` AS c
-       INNER JOIN `states` AS s
-       ON c.`state_id` = s.`id`
- ORDER BY c.`id`;
+SELECT tv_shows.title, tv_show_genres.genre_id 
+  FROM tv_shows 
+ INNER JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+ ORDER BY tv_shows.title, tv_show_genres.genre_id;
